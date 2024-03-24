3aa55fe3b131e2097d2e2a040dafce627ab65eac
viviana-lopes <viviana.lopes@dcmail.ca>
2024-03-23 20:10:51 -0400

    updated

--- azure/main.tf
+++ azure/main.tf
@@ -8,12 +8,27 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
+provider "azurerm" {
+features {}
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+}
+
+resource "azurerm_resource_group" "my_resource_group" {
+  name     = "lopes-resource-group"
+  location = "East US"
+  tags = {
+    environment = "production"
+  }
+}
 # Create VNet
 resource "azurerm_virtual_network" "my_vnet" {
-  name                = "lopes-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "East US" 
-  resource_group_name = "lopes-resource-group"
+  name = "lopes-vnet"
+  address_space= ["10.0.0.0/16"]
+  location= azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
 
   tags = {
     environment = "production"
