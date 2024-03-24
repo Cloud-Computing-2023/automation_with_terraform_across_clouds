a282c96ca6d6161891f9bc6d168762fdfaddab42
viviana-lopes <viviana.lopes@dcmail.ca>
2024-03-23 20:13:41 -0400

    addded

---
+++ azure/variables.tf
@@ -0,0 +1,15 @@
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Azure subscription ID"
+}
+
+variable "ARM_CLIENT_ID" {
+  description = "Azure client ID"
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "Azure client secret"
+}
+
+variable "ARM_TENANT_ID" {
+  description = "Azure tenant ID"
+}
